1066be358ad51dd8e9e6c7f91f35eecf9b35a769
Sahana Panda <sahana.panda@gmail.com>
2026-04-28 23:47:54 +0530

    Add list_problem.py for generating 3D coordinates
    Implement a solution to generate 3D coordinates based on user input.

---
+++ Day_2/list_problem.py
@@ -0,0 +1,17 @@
+# Day 2 - List Problem (3D Coordinates)
+# Problem: Generate all coordinates where i+j+k != n
+
+x = int(input())
+y = int(input())
+z = int(input())
+n = int(input())
+
+result = []
+
+for i in range(x+1):
+    for j in range(y+1):
+        for k in range(z+1):
+            if i + j + k != n:
+                result.append([i, j, k])
+
+print(result)
