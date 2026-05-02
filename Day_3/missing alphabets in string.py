666bf9cd3b9511919ea019c2d88af641c36141fb
Sahana Panda <sahana.panda@gmail.com>
2026-05-02 10:29:29 +0530

    Update print statement for missing letters output

--- Day_3/missing alphabets in string.py
+++ Day_3/missing alphabets in string.py
@@ -7,7 +7,7 @@ s = set(text)
 all_letters = set(string.ascii_lowercase + string.ascii_uppercase)
 missing = all_letters - s
 
-print("missing :", missing)
+print("Missing letters :", missing)
 
 abcdefghijklmnopqrstuvwABCDEFGHIJKLMNOPQRSTUVW
 missing : {'Y', 'Z', 'z', 'X', 'x', 'y'}
