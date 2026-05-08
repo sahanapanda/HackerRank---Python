48ec6bf2ad146ddec76d76ecab090c51a595c3d0
Sahana Panda <sahana.panda@gmail.com>
2026-05-08 20:42:01 +0530

    Clarify problem statement in leapyear.py

--- Day_2/leapyear.py
+++ Day_2/leapyear.py
@@ -1,5 +1,5 @@
 # Day 2 - Leap Year
-# Problem: Check whether a given year is a leap year
+# Problem: Check whether a given year is a leap year or not
 
 def is_leap(year):
     if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
