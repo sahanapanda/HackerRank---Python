30596fb529bdf3a1da178ffd6e221ca2dc263f1f
Sahana Panda <sahana.panda@gmail.com>
2026-05-15 22:46:34 +0530

    Add list operations functionality
    Implement list operations including insert, print, remove, append, sort, pop, and reverse based on user commands.

---
+++ Day_7/List Operations.py
@@ -0,0 +1,28 @@
+# Day 7 - List Operations
+# Problem: Perform different operations on a list
+
+lst = []
+
+for _ in range(int(input())):
+    command = input().split()
+
+    if command[0] == "insert":
+        lst.insert(int(command[1]), int(command[2]))
+
+    elif command[0] == "print":
+        print(lst)
+
+    elif command[0] == "remove":
+        lst.remove(int(command[1]))
+
+    elif command[0] == "append":
+        lst.append(int(command[1]))
+
+    elif command[0] == "sort":
+        lst.sort()
+
+    elif command[0] == "pop":
+        lst.pop()
+
+    elif command[0] == "reverse":
+        lst.reverse()
