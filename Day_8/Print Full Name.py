f9c0f95eb66b34c256193bb741600089c16acc2f
Sahana Panda <sahana.panda@gmail.com>
2026-05-17 11:39:56 +0530

    Add Print Full Name functionality

---
+++ Day_8/Print Full Name.py
@@ -0,0 +1,10 @@
+# Day 8 - Print Full Name
+# Problem: Print a greeting message using first and last name
+
+def print_full_name(first, last):
+    print(f"Hello {first} {last}! You just delved into python.")
+
+if __name__ == '__main__':
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)
