0a9b31f5eda29a40256564af0888820992e08c9a
Sahana Panda <sahana.panda@gmail.com>
2026-05-19 21:00:47 +0530

    Add string validators for various character properties
    Implement string validators to check alphanumeric, alphabetic, digit, lowercase, and uppercase properties.

---
+++ Day_9/String Validators.py
@@ -0,0 +1,15 @@
+# Day 9 - String Validators
+# Problem: Check different properties of characters in a string
+
+if __name__ == '__main__':
+    s = input()
+
+    print(any(char.isalnum() for char in s))
+
+    print(any(char.isalpha() for char in s))
+
+    print(any(char.isdigit() for char in s))
+
+    print(any(char.islower() for char in s))
+
+    print(any(char.isupper() for char in s))
